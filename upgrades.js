718ca5b6d3b871b8cf3e9d388a8ac96484eb678b
bryce <50299051+bryce-seifert@users.noreply.github.com>
2023-05-22 22:00:37 -0500

    feat: add separate h/v values to scale (#32)
    * Feat: add separate h/v values to scale
    
    * chore: version up

--- upgrades.js
+++ upgrades.js
@@ -4,4 +4,22 @@ export default [
 	CreateConvertToBooleanFeedbackUpgradeScript({
 		playStatus: true,
 	}),
+	function v3_2_0(context, props) {
+		let changes = {
+			updatedConfig: null,
+			updatedActions: [],
+			updatedFeedbacks: [],
+		}
+
+		for (const action of props.actions) {
+			if (action.actionId === 'scale') {
+				action.options.splitScale = false
+				action.options.valueH = 100
+				action.options.valueV = 100
+				changes.updatedActions.push(action)
+			}
+		}
+
+		return changes
+	},
 ]
